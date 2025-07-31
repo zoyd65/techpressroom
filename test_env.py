663fe7aedc29842e4d971894d77ca0337bbc1322
zoyd <pressarchivio@gmail.com>
2025-07-31 10:01:34 +0200

    Create test_env.py

---
+++ test_env.py
@@ -0,0 +1,5 @@
+import os
+
+print("TELEGRAM_BOT_TOKEN:", os.getenv("TELEGRAM_BOT_TOKEN"))
+print("TELEGRAM_USER_ID:", os.getenv("TELEGRAM_USER_ID"))
+print("HUGGINGFACE_API_KEY:", os.getenv("HUGGINGFACE_API_KEY"))
